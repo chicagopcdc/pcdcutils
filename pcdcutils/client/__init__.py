fbce6aa056f4a7c39d3491fa435bed5ce45be4bd
grugna <graglia01@gmail.com>
2024-02-09 11:33:48 -0600

    use gen3 sdk instead

--- pcdcutils/client/__init__.py
+++ pcdcutils/client/__init__.py
@@ -1,14 +1,19 @@
-import requests
-import base64
-import urllib.parse
+# import requests
+# import base64
+# import urllib.parse
+
+from gen3.auth import Gen3Auth
 
 
 class FenceClientManager(object):
 
-    def __init__(self, fence_url=None, client_id=None, client_secret=None):
-        self.fence_url = fence_url
+    def __init__(self, base_url=None, client_id=None, client_secret=None):
+        self.base_url = base_url
         self.client_id = client_id
         self.client_secret = client_secret
+
+        self.scopes = "openid user" #"user data openid"
+        self.auth = None
 
 
     def is_valid(self):
@@ -19,36 +24,39 @@ class FenceClientManager(object):
 
         return True
 
+    def authenticate(self):
+        if self.is_valid():
+            self.auth = Gen3Auth(
+                endpoint="https://portal-dev.pedscommons.org",
+                client_credentials=(self.client_id, self.client_secret),
+                client_scopes = self.scopes
+            )
 
 
     def get_auth_token(self):
-        '''
-        Returns bool if Signature header is present
-        '''
-        if self.is_valid():
-            url = self.fence_url + "/oauth2/token?grant_type=client_credentials"
-            scopes = "openid user"
-            #url encode the value
-            payload = "scope=" + urllib.parse.quote(scopes)
+        return self.auth.get_access_token()
+        # url = self.base_url + "user/oauth2/token?grant_type=client_credentials"
+        # 
+        # #url encode the value
+        # payload = "scope=" + urllib.parse.quote(self.scopes)
 
-            auth_str = client_id + ":" + client_secret
-            #base64 encode the credentials
-            auth_str_bytes = auth_str.encode("ascii") 
-            auth_base64_bytes = base64.b64encode(auth_str_bytes) 
-            auth_base64_string = auth_base64_bytes.decode("ascii") 
+        # auth_str = client_id + ":" + client_secret
+        # #base64 encode the credentials
+        # auth_str_bytes = auth_str.encode("ascii") 
+        # auth_base64_bytes = base64.b64encode(auth_str_bytes) 
+        # auth_base64_string = auth_base64_bytes.decode("ascii") 
 
-            headers = {
-              'Content-Type': 'application/x-www-form-urlencoded',
-              'Authorization': 'Basic ' + auth_base64_string,
-            }
+        # headers = {
+        #   'Content-Type': 'application/x-www-form-urlencoded',
+        #   'Authorization': 'Basic ' + auth_base64_string,
+        # }
 
-            response = requests.request("POST", url, headers=headers, data=payload)
-            if response.status_code == 200:
-                json_response = response.json()
-                return json_response["access_token"]
-            else:
-                print("ERROR!")
-                print(response.text)
-
-        return None
+        # response = requests.request("POST", url, headers=headers, data=payload)
+        # if response.status_code == 200:
+        #     json_response = response.json()
+        #     return json_response["access_token"]
+        # else:
+        #     print("ERROR!")
+        #     print(response.text)
+        # return None
 
