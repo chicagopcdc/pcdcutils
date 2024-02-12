d110b05f87eab8d55916c0a660734106be66408b
grugna <graglia01@gmail.com>
2024-02-12 11:26:11 -0600

    Update __init__.py

--- pcdcutils/client/__init__.py
+++ pcdcutils/client/__init__.py
@@ -78,7 +78,7 @@ class FenceClientManager(object):
                 )
             except TimeoutError:
                 # TODO send notification to 
-                print(f"TIMEOUT: Connection with client_credential to {config.PCDC_COMMON_BASENAME}/user failed.")
+                print(f"TIMEOUT: Connection with client_credential to {self.base_url}/user failed.")
             except Gen3AuthError as err:
                 print(f"AUTH ERROR: {err}")
 
