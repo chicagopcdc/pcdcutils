2a0c24003ee489d22d026ae82f5d00d81299a3f5
pkellyc <pkellyc@PED-ML-31.local>
2025-06-04 22:52:19 -0500

    pcdcutils tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,10 +1,10 @@
-from Crypto.PublicKey import RSA 
-from Crypto.Signature import pkcs1_15 
-from Crypto.Hash import SHA256 
-from types import SimpleNamespace
+from Crypto.PublicKey import RSA
+from Crypto.Signature import pkcs1_15
+from Crypto.Hash import SHA256
 import pytest
-from pcdcutils.gen3 import Gen3RequestManager
+from pcdcutils.gen3 import Gen3RequestManager, SignaturePayload
 import os
+import logging
 
 # openssl genpkey -algorithm RSA -out PRIVATE_NAME.pem -pkeyopt rsa_keygen_bits:2048
 # openssl rsa -pubout -in PRIVATE_NAME.pem -out PUB_NAME.pem
@@ -12,96 +12,121 @@ import os
 
 
 def test_successful_make_sig():
-  body = 'aaaaa'
-  pri_key_path = os.getcwd() + '/tests/keys/private_key1.pem'
-  keyfile = open(pri_key_path, "r").read()
-  pri_key = RSA.import_key(keyfile)
-  g3 = Gen3RequestManager({"Signature": None, "Gen3-Service": "service"})
-  sig = g3.make_gen3_signature(body, {"SERVICE_PRIVATE_KEY": pri_key})
-  assert sig == "4abceb628848385d3d8f03c1530bbd3d72cb194a6fe6ea20a54a7d2004a4dce16bfe9e46e0a73ddf7388482550ff135ff039bcb0cebc8fc5a095e2021c20cc0f7bfc5d80ab77cb4ad9f4603857e2781eb0d55279a9198a5048814c5cbd5e7fdc75f9722754bf11b87a9fa9703008fa3757bdeac644a46331a51d2b88d410bcf0"
+    payload = SignaturePayload(
+        method="POST",
+        path="/test/path",
+        body='{"key": "value"}',  # Any string like body = "aaaaa"
+    )
+    pri_key_path = os.getcwd() + "/tests/keys/private_key1.pem"
+    keyfile = open(pri_key_path, "r").read()
+    pri_key = RSA.import_key(keyfile)
+
+    g3 = Gen3RequestManager({"Signature": None, "Gen3-Service": "service"})
+    signature = g3.make_gen3_signature(payload, {"SERVICE_PRIVATE_KEY": pri_key})
+
+    assert isinstance(signature, str)
+    assert len(signature) > 10
 
 
 def test_bad_make_sig():
-  body = 'aaaaa'
-  bad_key_path = os.getcwd() + '/tests/keys/public_key1.pem'
-  keyfile = open(bad_key_path, "r").read()
-  bad_key = RSA.import_key(keyfile)
-  g3 = Gen3RequestManager({"Signature": None, "Gen3-Service": "service"})
-  with pytest.raises(TypeError) as ex:
-    sig = g3.make_gen3_signature(body, {"SERVICE_PRIVATE_KEY": bad_key})
-  assert "This is not a private key" in str(ex.value)
+    payload = SignaturePayload(
+        method="POST",
+        path="/test/path",
+        body='{"key": "value"}',  # Any string like body = "aaaaa"
+    )
+    bad_key_path = os.getcwd() + "/tests/keys/public_key1.pem"
+    keyfile = open(bad_key_path, "r").read()
+    bad_key = RSA.import_key(keyfile)
+
+    g3 = Gen3RequestManager({"Signature": None, "Gen3-Service": "service"})
+    with pytest.raises(TypeError) as ex:
+        g3.make_gen3_signature(payload, {"SERVICE_PRIVATE_KEY": bad_key})
+
+    assert "This is not a private key" in str(ex.value)
 
 
 def test_successful_validate_sig():
-  body = 'aaaaa'
-  pri_key_path = os.getcwd() + '/tests/keys/private_key1.pem'
-  keyfile = open(pri_key_path, "r").read()
-  pri_key = RSA.import_key(keyfile)
-  g3 = Gen3RequestManager({"Signature": None, "Gen3-Service": "service"})
-  sig = g3.make_gen3_signature(body, {"SERVICE_PRIVATE_KEY": pri_key})
-  signed_g3 = Gen3RequestManager({"Signature": "signature " + sig, "Gen3-Service": "service"})
-  pub_key_path = os.getcwd() + '/tests/keys/public_key1.pem'
-  keyfile = open(pub_key_path, "r").read()
-  pub_key = RSA.import_key(keyfile)
-  assert signed_g3.valid_gen3_signature(body, {"SERVICE_PUBLIC_KEY": pub_key})
+    payload = SignaturePayload(
+        method="POST",
+        path="/test/path",
+        body='{"key": "value"}',  # Any string like body = "aaaaa"
+    )
+    pri_key_path = os.getcwd() + "/tests/keys/private_key1.pem"
+    keyfile = open(pri_key_path, "r").read()
+    pri_key = RSA.import_key(keyfile)
+
+    g3 = Gen3RequestManager({"Signature": None, "Gen3-Service": "service"})
+    signature = g3.make_gen3_signature(payload, {"SERVICE_PRIVATE_KEY": pri_key})
+
+    signed_g3 = Gen3RequestManager(
+        {"Signature": "signature " + signature, "Gen3-Service": "service"}
+    )
+
+    pub_key_path = os.getcwd() + "/tests/keys/public_key1.pem"
+    keyfile = open(pub_key_path, "r").read()
+    pub_key = RSA.import_key(keyfile)
+
+    assert signed_g3.valid_gen3_signature(payload, {"SERVICE_PUBLIC_KEY": pub_key})
 
 
 def test_bad_validate_sig():
-  #Use wrong public key for the private key
-  body = 'aaaaa'
-  pri_key_path = os.getcwd() + '/tests/keys/private_key2.pem'
-  keyfile = open(pri_key_path, "r").read()
-  pri_key = RSA.import_key(keyfile)
-  g3 = Gen3RequestManager({"Signature": None, "Gen3-Service": "service"})
-  sig = g3.make_gen3_signature(body, {"SERVICE_PRIVATE_KEY": pri_key})
-  signed_g3 = Gen3RequestManager({"Signature": "signature " + sig, "Gen3-Service": "service"})
-  pub_key_path = os.getcwd() + '/tests/keys/public_key1.pem'
-  keyfile = open(pub_key_path, "r").read()
-  pub_key = RSA.import_key(keyfile)
-  assert signed_g3.valid_gen3_signature(body, {"SERVICE_PUBLIC_KEY": pub_key}) == False
+    payload = SignaturePayload(
+        method="POST",
+        path="/test/path",
+        body='{"key": "value"}',  # Any string like body = "aaaaa"
+    )
+    pri_key_path = os.getcwd() + "/tests/keys/private_key2.pem"
+    keyfile = open(pri_key_path, "r").read()
+    pri_key = RSA.import_key(keyfile)
 
+    g3 = Gen3RequestManager({"Signature": None, "Gen3-Service": "service"})
+    signature = g3.make_gen3_signature(payload, {"SERVICE_PRIVATE_KEY": pri_key})
 
-def test_make_gen3_signature_with_request_object():
-    # Simulated production request
-    fake_request = SimpleNamespace(
-        method='POST',
-        path='/admin/users/selected',
-        get_data=lambda as_text=True: '{"usernames": ["someone"]}'
+    signed_g3 = Gen3RequestManager(
+        {"Signature": "signature " + signature, "Gen3-Service": "service"}
     )
 
-    # Generate a key
+    pub_key_path = os.getcwd() + "/tests/keys/public_key1.pem"
+    keyfile = open(pub_key_path, "r").read()
+    pub_key = RSA.import_key(keyfile)
+
+    assert (
+        signed_g3.valid_gen3_signature(payload, {"SERVICE_PUBLIC_KEY": pub_key})
+        == False
+    )
+
+
+def test_make_gen3_signature_with_signature_payload():
+    payload = SignaturePayload(
+        method="POST", path="/admin/users/selected", body='{"usernames": ["someone"]}'
+    )
+
     key = RSA.generate(2048)
 
-    headers = {
-        "Signature": None,
-        "Gen3-Service": "test-service"
-    }
+    g3 = Gen3RequestManager({"Signature": None, "Gen3-Service": "test-service"})
+    config = {"TEST-SERVICE_PRIVATE_KEY": key}
 
-    g3 = Gen3RequestManager(headers)
-    config = {
-        "TEST-SERVICE_PRIVATE_KEY": key
-    }
-
-    signature = g3.make_gen3_signature(fake_request, config)
+    signature = g3.make_gen3_signature(payload, config)
 
     assert isinstance(signature, str)
-    assert len(signature) > 10  # sanity check
+    assert len(signature) > 10
+
 
 def test_signature_logs(caplog):
-    # Simulate a Flask-like request object
-    fake_request = SimpleNamespace(
-        method='POST',
-        path='/admin/users/selected',
-        get_data=lambda as_text=True: '{"usernames": ["someone"]}'
+    payload = SignaturePayload(
+        method="POST", path="/admin/users/selected", body='{"usernames": ["someone"]}'
     )
 
-    # Generate a temporary private key for the test
     test_key = RSA.generate(2048)
 
-    # Create the Gen3RequestManager instance
     g3 = Gen3RequestManager({"Signature": None, "Gen3-Service": "test-service"})
+    config = {"TEST-SERVICE_PRIVATE_KEY": test_key}
 
-    # Provide a config dict with the test private key
-    config = {
-        "TEST-SERVICE_PRIVATE_KEY": test_key
-    }
+    # Checking caplog is actually captured.
+    with caplog.at_level(logging.INFO):
+        signature = g3.make_gen3_signature(payload, config)
+
+    # Check for the log message
+    assert any(
+        "signed payload of length" in record.message for record in caplog.records
+    )
